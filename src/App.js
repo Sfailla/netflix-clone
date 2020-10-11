63a19da30c55268df3643bd650816ea1082fbcac
Stevo99 <Sfailla1983@gmail.com>
2020-10-11 10:58:24 -0400

    added jumbotron styled-components to App.js

--- src/App.js
+++ src/App.js
@@ -1,8 +1,23 @@
 import React from 'react';
+import JumbotronData from './fixtures/jumbo.json';
 import Jumbotron from './components/jumbotron';
 
 function App() {
-	return <Jumbotron />;
+	return (
+		<Jumbotron.Container>
+			{JumbotronData.map(item => (
+				<Jumbotron key={item.id} direction={item.direction}>
+					<Jumbotron.Pane>
+						<Jumbotron.Title>{item.title}</Jumbotron.Title>
+						<Jumbotron.Subtitle>{item.subtitle}</Jumbotron.Subtitle>
+					</Jumbotron.Pane>
+					<Jumbotron.Pane>
+						<Jumbotron.Image src={item.image} alt={item.alt} />
+					</Jumbotron.Pane>
+				</Jumbotron>
+			))}
+		</Jumbotron.Container>
+	);
 }
 
 export default App;
