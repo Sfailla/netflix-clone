c635c63095190aeedd7fa625a161c9f80f841921
Stevo99 <Sfailla1983@gmail.com>
2020-10-11 10:59:41 -0400

    added Pane Subtitle Title and Image Components to Jumbotron Component

--- src/components/jumbotron/index.js
+++ src/components/jumbotron/index.js
@@ -1,7 +1,32 @@
 import React from 'react';
+import { Container, Inner, Pane, Title, Subtitle, Image } from './styles/jumbotron';
 
-function Jumbotron({}) {
-	return <p>I am Jumbotron</p>;
+function Jumbotron({ children, direction = 'row', ...restProps }) {
+	return (
+		<Inner {...restProps} direction={direction}>
+			{children}
+		</Inner>
+	);
 }
+
+Jumbotron.Container = function JumbotronContainer({ children, ...restProps }) {
+	return <Container {...restProps}>{children}</Container>;
+};
+
+Jumbotron.Pane = function JumbotronPane({ children, ...restProps }) {
+	return <Pane {...restProps}>{children}</Pane>;
+};
+
+Jumbotron.Title = function JumbotronTitle({ children, ...restProps }) {
+	return <Title {...restProps}>{children}</Title>;
+};
+
+Jumbotron.Subtitle = function JumbotronSubtitle({ children, ...restProps }) {
+	return <Subtitle {...restProps}>{children}</Subtitle>;
+};
+
+Jumbotron.Image = function JumbotronImage({ ...restProps }) {
+	return <Image {...restProps} />;
+};
 
 export default Jumbotron;
