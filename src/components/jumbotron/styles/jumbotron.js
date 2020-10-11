0bec2e29466378001c5da1d8be7f886f83356e41
Stevo99 <Sfailla1983@gmail.com>
2020-10-11 11:00:09 -0400

    added styles for all Jumbotron components

---
+++ src/components/jumbotron/styles/jumbotron.js
@@ -0,0 +1,22 @@
+import styled from 'styled-components/macro';
+
+export const Inner = styled.div`
+	display: flex;
+	flex-direction: ${({ direction }) => direction};
+	align-items: center;
+	justify-content: space-between;
+	max-width: 1100px;
+	margin: auto;
+	width: 100%;
+
+	@media (max-width: 1000px) {
+		flex-direction: column;
+	}
+`;
+
+export const Container = styled.div``;
+
+export const Pane = styled.div`width: 50%;`;
+export const Title = styled.h1``;
+export const Subtitle = styled.h2``;
+export const Image = styled.img``;
