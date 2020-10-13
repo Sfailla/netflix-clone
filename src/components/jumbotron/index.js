7e4b94befdc551aff238c4ee7bdd85432eb26e3f
Stevo99 <Sfailla1983@gmail.com>
2020-10-12 23:16:27 -0400

    created all sub-components for jumbotron

--- src/components/jumbotron/index.js
+++ src/components/jumbotron/index.js
@@ -1,11 +1,31 @@
 import React from 'react';
-import { Container, Inner, Pane, Title, Subtitle, Image } from './styles/jumbotron';
+import {
+	Container,
+	Item,
+	Inner,
+	Pane,
+	Title,
+	Subtitle,
+	Card,
+	CardImage,
+	CardTitle,
+	CardSubtitle,
+	Icon,
+	Text,
+	Image,
+	Video,
+	Source,
+	ImageContainer,
+	ImageOverlay
+} from './styles/jumbotron';
 
-function Jumbotron({ children, direction = 'row', ...restProps }) {
+function Jumbotron({ children, direction = 'row', overlay, ...restProps }) {
 	return (
-		<Inner {...restProps} direction={direction}>
-			{children}
-		</Inner>
+		<Item>
+			<Inner {...restProps} direction={direction}>
+				{children}
+			</Inner>
+		</Item>
 	);
 }
 
@@ -25,8 +45,54 @@ Jumbotron.Subtitle = function JumbotronSubtitle({ children, ...restProps }) {
 	return <Subtitle {...restProps}>{children}</Subtitle>;
 };
 
+Jumbotron.ImageContainer = function JumbotronImageContainer({
+	children,
+	...restProps
+}) {
+	return <ImageContainer {...restProps}>{children}</ImageContainer>;
+};
+
 Jumbotron.Image = function JumbotronImage({ ...restProps }) {
 	return <Image {...restProps} />;
+};
+
+Jumbotron.Card = function JumbotronImageCard({ children, ...restProps }) {
+	return <Card {...restProps}>{children}</Card>;
+};
+
+Jumbotron.CardImage = function JumbotronImage({ ...restProps }) {
+	return <CardImage {...restProps} />;
+};
+
+Jumbotron.CardTitle = function JumbotronCardTitle({ children, ...restProps }) {
+	return <CardTitle {...restProps}>{children}</CardTitle>;
+};
+
+Jumbotron.CardSubtitle = function JumbotronCardSubtitle({ children, ...restProps }) {
+	return <CardSubtitle {...restProps}>{children}</CardSubtitle>;
+};
+
+Jumbotron.Text = function JumbotronCardText({ children, ...restProps }) {
+	return <Text {...restProps}>{children}</Text>;
+};
+
+Jumbotron.AnimatedIcon = function JumbotronCardAnimation({
+	children,
+	...restProps
+}) {
+	return <Icon {...restProps} />;
+};
+
+Jumbotron.Overlay = function JumbotronImageOverlay({ children, ...restProps }) {
+	return <ImageOverlay {...restProps}>{children}</ImageOverlay>;
+};
+
+Jumbotron.Video = function JumbotronVideo({ children, ...restProps }) {
+	return <Video {...restProps}>{children}</Video>;
+};
+
+Jumbotron.Source = function JumbotronVideoSource({ ...restProps }) {
+	return <Source {...restProps} />;
 };
 
 export default Jumbotron;
