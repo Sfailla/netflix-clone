5a04c58297b29f80f58265781c6a9c3e279cb555
Stevo99 <Sfailla1983@gmail.com>
2020-10-12 23:15:30 -0400

    added all Jumbotron style sub components to App.js

--- src/App.js
+++ src/App.js
@@ -12,7 +12,32 @@ function App() {
 						<Jumbotron.Subtitle>{item.subtitle}</Jumbotron.Subtitle>
 					</Jumbotron.Pane>
 					<Jumbotron.Pane>
-						<Jumbotron.Image src={item.image} alt={item.alt} />
+						<Jumbotron.ImageContainer>
+							<Jumbotron.Image src={item.image} alt={item.alt} />
+							{item.videoCard ? (
+								<Jumbotron.Overlay
+									maxWidth={item.maxWidth}
+									maxHeight={item.maxHeight}
+									top={item.top}
+								>
+									<Jumbotron.Video autoPlay loop muted playsInline>
+										<Jumbotron.Source src={item.video} type="video/mp4" />
+									</Jumbotron.Video>
+								</Jumbotron.Overlay>
+							) : null}
+							{item.imageCard ? (
+								<Jumbotron.Card>
+									<Jumbotron.CardImage src={item.cardImage} />
+									<Jumbotron.Text>
+										<Jumbotron.CardTitle>{item.cardTitle}</Jumbotron.CardTitle>
+										<Jumbotron.CardSubtitle>
+											{item.cardSubtitle}
+										</Jumbotron.CardSubtitle>
+									</Jumbotron.Text>
+									<Jumbotron.AnimatedIcon />
+								</Jumbotron.Card>
+							) : null}
+						</Jumbotron.ImageContainer>
 					</Jumbotron.Pane>
 				</Jumbotron>
 			))}
