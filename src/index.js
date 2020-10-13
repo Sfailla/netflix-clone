c260731f378011b1dc1ebe5326c01c750b124e4e
Stevo99 <Sfailla1983@gmail.com>
2020-10-12 23:17:52 -0400

    added global styles to index.js

--- src/index.js
+++ src/index.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import 'normalize.css';
+import { GlobalStyles } from './global-styles';
 import App from './App';
 
 ReactDOM.render(
 	<React.StrictMode>
+		<GlobalStyles />
 		<App />
 	</React.StrictMode>,
 	document.getElementById('root')
