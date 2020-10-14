df67414ce1cdb45bee52e01fa2b689845b4e4286
Stevo99 <Sfailla1983@gmail.com>
2020-10-14 12:52:55 -0400

    added footer component to App.js

--- src/App.js
+++ src/App.js
@@ -1,47 +1,16 @@
 import React from 'react';
-import JumbotronData from './fixtures/jumbo.json';
-import Jumbotron from './components/jumbotron';
+import Jumbotron from './containers/jumbotron';
+import { BrowserRouter as Router, Switch } from 'react-router-dom';
+import Footer from './containers/footer';
 
 function App() {
 	return (
-		<Jumbotron.Container>
-			{JumbotronData.map(item => (
-				<Jumbotron key={item.id} direction={item.direction}>
-					<Jumbotron.Pane>
-						<Jumbotron.Title>{item.title}</Jumbotron.Title>
-						<Jumbotron.Subtitle>{item.subtitle}</Jumbotron.Subtitle>
-					</Jumbotron.Pane>
-					<Jumbotron.Pane>
-						<Jumbotron.ImageContainer>
-							<Jumbotron.Image src={item.image} alt={item.alt} />
-							{item.videoCard ? (
-								<Jumbotron.Overlay
-									maxWidth={item.maxWidth}
-									maxHeight={item.maxHeight}
-									top={item.top}
-								>
-									<Jumbotron.Video autoPlay loop muted playsInline>
-										<Jumbotron.Source src={item.video} type="video/mp4" />
-									</Jumbotron.Video>
-								</Jumbotron.Overlay>
-							) : null}
-							{item.imageCard ? (
-								<Jumbotron.Card>
-									<Jumbotron.CardImage src={item.cardImage} />
-									<Jumbotron.Text>
-										<Jumbotron.CardTitle>{item.cardTitle}</Jumbotron.CardTitle>
-										<Jumbotron.CardSubtitle>
-											{item.cardSubtitle}
-										</Jumbotron.CardSubtitle>
-									</Jumbotron.Text>
-									<Jumbotron.AnimatedIcon />
-								</Jumbotron.Card>
-							) : null}
-						</Jumbotron.ImageContainer>
-					</Jumbotron.Pane>
-				</Jumbotron>
-			))}
-		</Jumbotron.Container>
+		<Router>
+			<Switch>
+				<Jumbotron />
+			</Switch>
+			<Footer />
+		</Router>
 	);
 }
 
