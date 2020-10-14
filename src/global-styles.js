d02b3b336d242b91256474454a9ace661a710db1
Stevo99 <Sfailla1983@gmail.com>
2020-10-14 12:54:20 -0400

    added box-style: border-box to global styles

--- src/global-styles.js
+++ src/global-styles.js
@@ -1,6 +1,14 @@
 import { createGlobalStyle } from 'styled-components';
 
 export const GlobalStyles = createGlobalStyle`
+  *,
+  *::before,
+  *::after {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+  }
+  
   html,
   body {
     font-family: Helvetica Neue, Helvetica, Arial, sans-serif;
