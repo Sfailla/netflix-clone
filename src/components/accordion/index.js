c62e9fbbde3658b32c7c528c2e4bae36b190e421
Stevo99 <Sfailla1983@gmail.com>
2020-10-15 11:17:00 -0400

    created accordion component and styles

---
+++ src/components/accordion/index.js
@@ -0,0 +1,84 @@
+import React from 'react';
+
+import {
+	Container,
+	Inner,
+	Title,
+	Frame,
+	Item,
+	Header,
+	Body,
+	Content,
+	Svg
+} from './styles/accordion';
+
+const ToggleContext = React.createContext();
+
+export default function Accordion({ children, maxHeight, ...restProps }) {
+	return (
+		<Container {...restProps}>
+			<Inner>{children}</Inner>
+		</Container>
+	);
+}
+
+Accordion.Title = function AccordionTitle({ children, ...restProps }) {
+	return <Title {...restProps}>{children}</Title>;
+};
+
+Accordion.Frame = function AccordionFrame({ children, ...restProps }) {
+	return <Frame {...restProps}>{children}</Frame>;
+};
+
+Accordion.Item = function AccordionItem({ children, ...restProps }) {
+	const [ state, setState ] = React.useState({
+		toggle: false,
+		maxHeight: 0
+	});
+
+	return (
+		<ToggleContext.Provider value={{ state, setState }}>
+			<Item {...restProps}>{children}</Item>
+		</ToggleContext.Provider>
+	);
+};
+
+Accordion.Svg = function AccordionSvg({ children, ...restProps }) {
+	return <Svg {...restProps}>{children}</Svg>;
+};
+
+Accordion.Header = function AccordionHeader({ children, onClick, ...restProps }) {
+	const { state, setState } = React.useContext(ToggleContext);
+	const { toggle } = state;
+	return (
+		<Header
+			onClick={() => {
+				setState({
+					toggle: !toggle,
+					maxHeight: toggle ? 0 : 1200
+				});
+			}}
+			{...restProps}
+		>
+			{children}
+			<Accordion.Svg toggle={toggle} viewBox="0 0 30 30">
+				<path d="M23.73 16.23h-7.5v7.5h-2.461v-7.5h-7.5v-2.461h7.5v-7.5h2.461v7.5h7.5v2.461z" />
+			</Accordion.Svg>
+		</Header>
+	);
+};
+
+Accordion.Body = function AccordionBody({ children, ...restProps }) {
+	const { state } = React.useContext(ToggleContext);
+	const { maxHeight, toggle } = state;
+	console.log(toggle);
+	return (
+		<Body maxHeight={maxHeight} {...restProps}>
+			{children}
+		</Body>
+	);
+};
+
+Accordion.Content = function AccordionContent({ children, ...restProps }) {
+	return <Content {...restProps}>{children}</Content>;
+};
---
+++ src/components/accordion/styles/accordion.js
@@ -0,0 +1,119 @@
+import styled from 'styled-components/macro';
+
+export const Container = styled.div`
+	padding: 70px 45px;
+	text-align: center;
+	margin: 0 auto;
+	position: relative;
+	border-bottom: 8px solid #333;
+`;
+
+export const Title = styled.h1`
+	font-size: 50px;
+	line-height: 1.1;
+	margin-top: 0;
+	margin-bottom: 8px;
+	color: white;
+	text-align: center;
+	letter-spacing: 5px;
+
+	@media (max-width: 949px) {
+		font-size: 40px;
+	}
+
+	@media (max-width: 740px) {
+		font-size: 38px;
+	}
+
+	@media (max-width: 600px) {
+		font-size: 35px;
+	}
+
+	@media (max-width: 549px) {
+		font-size: 26px;
+	}
+`;
+
+export const Inner = styled.div`
+	display: flex;
+	flex-direction: column;
+	margin: auto;
+`;
+
+export const Frame = styled.div`
+	width: 75%;
+	max-width: 815px;
+	margin: 32px auto;
+
+	@media (max-width: 950px) {
+		width: 90%;
+		margin: 24px auto;
+	}
+`;
+
+export const Item = styled.div`
+  margin-bottom: 8px;
+  position: relative;
+  width: 100%;
+  border: 0;
+
+  display: block;
+  text-align: left;
+  background: #000;
+}
+`;
+
+export const Header = styled.div`
+	display: flex;
+	align-items: center;
+	justify-content: space-between;
+	text-transform: uppercase;
+	margin-bottom: 1px;
+	padding: .8em 2.2em .8em 1.2em;
+	background-color: #303030;
+	color: white;
+	font-size: 26px;
+	font-weight: 400;
+
+	@media (max-width: 949px) {
+		font-size: 20px;
+	}
+
+	@media (max-width: 650px) {
+		font-size: 18px;
+	}
+`;
+
+export const Body = styled.div`
+	display: block;
+	font-size: 23px;
+	max-height: ${({ maxHeight }) => maxHeight}px;
+	color: white;
+	background-color: #303030;
+	overflow: hidden;
+
+	transition: max-height .5s cubic-bezier(.5, 0, .1, 1);
+
+	@media (max-width: 650px) {
+		font-size: 18px;
+	}
+`;
+
+export const Content = styled.span`
+	width: 100%;
+	height: 100%;
+	display: block;
+	padding: 19.2px;
+`;
+
+export const Svg = styled.svg.attrs({
+	version: '1.1',
+	xmlns: 'http://www.w3.org/2000/svg',
+	xmlnsXlink: 'http://www.w3.org/1999/xlink'
+})`
+  width: 30px;
+  height: 30px;
+  fill: #FFF;
+  transform: ${({ toggle }) => (toggle ? 'rotate(-45deg)' : 'rotate(0deg)')};
+  transition: transform .5s cubic-bezier(.5, 0, .1, 1);
+`;
