70f3c2be55b680c11ceee7486d7d1a6ff90d20d7
Stevo99 <Sfailla1983@gmail.com>
2020-10-17 11:18:23 -0400

    created and added OptForm component to components and App.js files

--- src/components/opt-form/index.js
+++ src/components/opt-form/index.js
@@ -1,18 +1,47 @@
 import React from 'react';
-import { Container, Input, Button, Svg } from './styles/opt-form';
+import {
+	Container,
+	Label,
+	Input,
+	Text,
+	Button,
+	Wrapper,
+	EmailWrapper,
+	Svg
+} from './styles/opt-form';
 
 export default function OptForm({ children, ...restProps }) {
 	return <Container {...restProps}>{children}</Container>;
 }
 
-OptForm.Input = function OptFormInput({ ...restProps }) {
-	return <Input {...restProps} />;
+OptForm.Label = function OptFormLabel({ children, ...restProps }) {
+	return <Label {...restProps}>{children}</Label>;
+};
+
+OptForm.Wrapper = function OptFormWrapper({ children, ...restProps }) {
+	return <Wrapper {...restProps}>{children}</Wrapper>;
+};
+
+OptForm.EmailWrapper = function OptFormEmailWrapper({ children, ...restProps }) {
+	return <EmailWrapper {...restProps}>{children}</EmailWrapper>;
+};
+
+OptForm.Input = function OptFormInput({ children, ...restProps }) {
+	return <Input {...restProps}>{children}</Input>;
+};
+
+OptForm.Text = function OptFormText({ children, ...restProps }) {
+	return <Text {...restProps}>{children}</Text>;
 };
 
 OptForm.Button = function OptFormButton({ children, ...restProps }) {
 	return <Button {...restProps}>{children}</Button>;
 };
 
-OptForm.Svg = function OptFormSvg({ children, ...restProps }) {
-	return <Svg {...restProps}>{children}</Svg>;
+OptForm.Svg = function OptFormSvg({ d, ...restProps }) {
+	return (
+		<Svg {...restProps}>
+			<path d={d} />
+		</Svg>
+	);
 };
--- src/components/opt-form/styles/opt-form.js
+++ src/components/opt-form/styles/opt-form.js
@@ -1,6 +1,118 @@
 import styled from 'styled-components/macro';
 
-export const Container = styled.div``;
-export const Input = styled.input``;
-export const Button = styled.button``;
-export const Svg = styled.div``;
+export const Container = styled.div`
+	width: 100%;
+	height: 100%;
+	display: flex;
+	justify-content: center;
+	margin-top: 20px;
+	flex-wrap: wrap;
+	flex-direction: column;
+	max-width: 950px;
+	margin: 0 auto;
+
+	@media (max-width: 1000px) {
+		align-items: center;
+	}
+`;
+
+export const Label = styled.label`
+	font-size: 16px;
+	color: #8c8c8c;
+
+	position: absolute;
+	top: 50%;
+	left: 10px;
+	transform: translateY(-50%);
+	transition: all .1s ease;
+`;
+
+export const Wrapper = styled.div`
+	display: flex;
+	justify-content: center;
+	align-items: center;
+	width: 100%;
+
+	@media (max-width: 800px) {
+		flex-direction: column;
+	}
+`;
+
+export const EmailWrapper = styled.div`
+	width: 500px;
+	height: auto;
+	position: relative;
+`;
+
+export const Input = styled.input`
+	width: 100%;
+	max-width: 500px;
+	padding: 10px;
+	height: 70px;
+	&:focus {
+		border-color: #0071eb;
+		outline: 0;
+	}
+	&:focus + label {
+		transform: translateY(0);
+		top: 6px;
+		font-weight: 700;
+		font-size: 13px;
+	}
+
+	@media (max-width: 800px) {
+		margin: 10px auto;
+		&:focus + label {
+			top: 17px;
+		}
+	}
+
+	@media (max-width: 750px) {
+		height: 48px;
+		padding-top: 20px;
+		&:focus + label {
+			top: 17px;
+			font-size: 11px;
+		}
+	}
+`;
+
+export const Button = styled.button`
+	min-height: 70px;
+	display: flex;
+	align-items: center;
+	height: 1000%;
+	background-color: #e50914;
+	color: white;
+	text-transform: uppercase;
+	padding: 0 32px;
+	font-size: 26px;
+	border: 0;
+	white-space: nowrap;
+	cursor: pointer;
+	&:hover {
+		background-color: #f40612;
+	}
+
+	@media (max-width: 800px) {
+		min-width: 74px;
+		min-height: 40px;
+		font-size: 16px;
+		margin: .25em auto;
+		padding: 0 1em;
+	}
+`;
+
+export const Svg = styled.svg`
+	fill: white;
+	height: 20px;
+	padding-left: 7px;
+`;
+
+export const Text = styled.p`
+	text-align: center;
+	font-size: 20px;
+	font-weight: 400;
+	padding-bottom: 20px;
+	color: white;
+`;
