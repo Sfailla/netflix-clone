12eadfafdf3a866bf3f5902860fb1ec5a6ad7ff8
Stevo99 <Sfailla1983@gmail.com>
2020-10-17 11:19:55 -0400

    added OptForm to faq container

--- src/containers/faq.js
+++ src/containers/faq.js
@@ -19,8 +19,22 @@ export default function AccordionContainer() {
 				))}
 			</Accordion.Frame>
 			<OptForm>
-				<OptForm.Input placeHolder="this is an input" />
-				<OptForm.Button>Click Me</OptForm.Button>
+				<OptForm.Text>
+					Ready to watch? Enter your email to create or restart your membership
+				</OptForm.Text>
+				<OptForm.Wrapper>
+					<OptForm.EmailWrapper>
+						<OptForm.Input />
+						<OptForm.Label placeholder="Email address">Email address</OptForm.Label>
+					</OptForm.EmailWrapper>
+					<OptForm.Button>
+						get started
+						<OptForm.Svg
+							viewBox="0 0 30 30"
+							d="M19.875 15l-10.711-11.13c-0.402-0.405-0.402-1.060 0-1.469 0.402-0.405 1.051-0.405 1.454 0l11.745 11.862c0.402 0.406 0.402 1.064 0 1.469l-11.745 11.862c-0.402 0.406-1.051 0.405-1.454 0-0.402-0.404-0.402-1.060 0-1.469l10.711-11.125z"
+						/>
+					</OptForm.Button>
+				</OptForm.Wrapper>
 			</OptForm>
 		</Accordion>
 	);
