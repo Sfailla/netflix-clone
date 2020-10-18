7f34eafbd6fce37d41346aebc2f5e21472bf9145
Stevo99 <Sfailla1983@gmail.com>
2020-10-18 14:55:06 -0400

    added hero component to index

--- src/components/index.js
+++ src/components/index.js
@@ -2,3 +2,5 @@ export { default as Jumbotron } from './jumbotron/index';
 export { default as Accordion } from './accordion/index';
 export { default as OptForm } from './opt-form/index';
 export { default as Footer } from './footer/index';
+export { default as Header } from './header/index';
+export { default as Hero } from './hero/index';
