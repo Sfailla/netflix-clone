80611226341fba336b4228fe8e4a2c90c8d853dc
Stevo99 <Sfailla1983@gmail.com>
2020-10-19 19:09:54 -0400

    added firebase context provider to app route

--- src/App.js
+++ src/App.js
@@ -1,16 +1,21 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import * as ROUTES from './constants/routes';
+
+import firebase, { FirebaseContext } from './firebase';
+
 import Home from './pages/Home';
 
 function App() {
 	return (
 		<Router>
-			<Switch>
-				<Route exact path={ROUTES.HOME}>
-					<Home />
-				</Route>
-			</Switch>
+			<FirebaseContext.Provider value={{ firebase }}>
+				<Switch>
+					<Route exact path={ROUTES.HOME}>
+						<Home />
+					</Route>
+				</Switch>
+			</FirebaseContext.Provider>
 		</Router>
 	);
 }
