26bfeb2a855cd53f5278e04061fe516073540306
Stevo99 <Sfailla1983@gmail.com>
2020-10-19 20:17:18 -0400

    created index for containers so multiple components can be called in single import statement

---
+++ src/containers/index.js
@@ -0,0 +1,4 @@
+export { default as Header } from './header';
+export { default as Jumbotron } from './jumbotron';
+export { default as Accordion } from './faq';
+export { default as Footer } from './footer';
