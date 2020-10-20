07fe5b2d1c804e91b64a5262c1bbc0203dc49fd5
Stevo99 <Sfailla1983@gmail.com>
2020-10-19 20:18:34 -0400

    created Browse SignIn and SignUp files for routes

---
+++ src/pages/Sign_In.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import { Header, Footer } from '../containers';
+
+export default function SignIn() {
+	return (
+		<React.Fragment>
+			<Header />
+			<Footer />
+		</React.Fragment>
+	);
+}
