2419d08f2164db5ea7f8dbea82d311dd77321c61
Sfailla <Sfailla1983@gmail.com>
2020-10-23 13:53:49 -0400

    changed name of accordion container from faq to accordion

--- src/containers/index.js
+++ src/containers/index.js
@@ -1,6 +1,6 @@
 export { default as Header } from './header';
 export { default as Jumbotron } from './jumbotron';
-export { default as Accordion } from './faq';
+export { default as Accordion } from './accordion';
 export { default as Footer } from './footer';
 export { default as SignIn } from './signin';
 export { default as SignUp } from './signup';
