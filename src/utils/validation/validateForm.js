ba1a915b6633581aa6ed9c1d42aa946c14651896
Sfailla <Sfailla1983@gmail.com>
2020-10-23 13:54:43 -0400

    refactored validateForm so it only checks for errors in valid form values

--- src/utils/validation/validateForm.js
+++ src/utils/validation/validateForm.js
@@ -2,16 +2,16 @@ export default function validateForm(values) {
 	let errors = {};
 
 	// Email Errors
-	if (!values.email) {
+	if (values.email === '') {
 		errors.email = 'Email is required';
 	} else if (!/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i.test(values.email)) {
 		errors.email = 'Invalid Email Address';
 	}
 
 	// Password Errors
-	if (!values.password) {
+	if (values.password === '') {
 		errors.password = 'Password is required';
-	} else if (values.password.length < 6) {
+	} else if (values.password && values.password.length < 6) {
 		errors.password = 'Password must be 6 characters';
 	}
 
