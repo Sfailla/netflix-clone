c581fc42b02413686e43e11ccd6cdbe0633f8ed1
Sfailla <Sfailla1983@gmail.com>
2020-10-23 12:18:01 -0400

    added nav prop to header and index container. added Signup to faq container

--- src/containers/faq.js
+++ src/containers/faq.js
@@ -3,7 +3,7 @@ import FaqData from '../fixtures/faqs.json';
 
 import AccordionStyles from '../components/accordion/accordionStyles';
 import Accordion from '../components/accordion';
-import { OptForm } from '../components';
+import { SignUp } from '../containers';
 
 export default function AccordionContainer() {
 	const [ activeEventKey, setActiveEventKey ] = React.useState(0);
@@ -34,24 +34,7 @@ export default function AccordionContainer() {
 					);
 				})}
 			</Accordion>
-			<OptForm>
-				<OptForm.Text>
-					Ready to watch? Enter your email to create or restart your membership
-				</OptForm.Text>
-				<OptForm.Wrapper>
-					<OptForm.EmailWrapper>
-						<OptForm.Input />
-						<OptForm.Label placeholder="Email address">Email address</OptForm.Label>
-					</OptForm.EmailWrapper>
-					<OptForm.Button>
-						get started
-						<OptForm.Svg
-							viewBox="0 0 30 30"
-							d="M19.875 15l-10.711-11.13c-0.402-0.405-0.402-1.060 0-1.469 0.402-0.405 1.051-0.405 1.454 0l11.745 11.862c0.402 0.406 0.402 1.064 0 1.469l-11.745 11.862c-0.402 0.406-1.051 0.405-1.454 0-0.402-0.404-0.402-1.060 0-1.469l10.711-11.125z"
-						/>
-					</OptForm.Button>
-				</OptForm.Wrapper>
-			</OptForm>
+			<SignUp />
 		</AccordionStyles>
 	);
 }
--- src/containers/index.js
+++ src/containers/index.js
@@ -3,3 +3,4 @@ export { default as Jumbotron } from './jumbotron';
 export { default as Accordion } from './faq';
 export { default as Footer } from './footer';
 export { default as SignIn } from './signin';
+export { default as SignUp } from './signup';
