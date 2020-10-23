6a13ffa31542f3b9365c0c8dbd215493fc05de1d
Sfailla <Sfailla1983@gmail.com>
2020-10-23 12:16:29 -0400

    added signup and signin to components index file

--- src/components/index.js
+++ src/components/index.js
@@ -1,7 +1,7 @@
 export { default as Jumbotron } from './jumbotron';
 export { default as Accordion } from './accordion';
-export { default as OptForm } from './opt-form';
+export { default as SignUp } from './signup';
 export { default as Footer } from './footer';
 export { default as Header } from './header';
 export { default as Hero } from './hero';
-export { default as SignIn } from './auth-form';
+export { default as SignIn } from './signin';
