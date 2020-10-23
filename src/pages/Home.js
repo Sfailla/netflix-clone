3e7472748b542e2217ac23b100b507f5a0618aad
Sfailla <Sfailla1983@gmail.com>
2020-10-23 12:18:39 -0400

    added nav prop to Home page

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,33 +1,16 @@
 import React from 'react';
-import { Hero, OptForm } from '../components';
-import { Jumbotron, Footer, Header, Accordion } from '../containers';
+import { Hero } from '../components';
+import { Jumbotron, Footer, Header, SignUp, Accordion } from '../containers';
 
 export default function Home() {
 	return (
 		<React.Fragment>
-			<Header bg={true}>
+			<Header bg={true} nav={true}>
 				<Hero>
 					<Hero.Card>
 						<Hero.Title>Unlimited movies, TV shows, and more.</Hero.Title>
 						<Hero.SubTitle>Watch anywhere. Cancel anytime.</Hero.SubTitle>
-						<OptForm>
-							<OptForm.Text>
-								Ready to watch? Enter your email to create or restart your membership
-							</OptForm.Text>
-							<OptForm.Wrapper>
-								<OptForm.EmailWrapper>
-									<OptForm.Input placeholder=" " />
-									<OptForm.Label>Email address</OptForm.Label>
-								</OptForm.EmailWrapper>
-								<OptForm.Button>
-									get started
-									<OptForm.Svg
-										viewBox="0 0 30 30"
-										d="M19.875 15l-10.711-11.13c-0.402-0.405-0.402-1.060 0-1.469 0.402-0.405 1.051-0.405 1.454 0l11.745 11.862c0.402 0.406 0.402 1.064 0 1.469l-11.745 11.862c-0.402 0.406-1.051 0.405-1.454 0-0.402-0.404-0.402-1.060 0-1.469l10.711-11.125z"
-									/>
-								</OptForm.Button>
-							</OptForm.Wrapper>
-						</OptForm>
+						<SignUp />
 					</Hero.Card>
 				</Hero>
 			</Header>
