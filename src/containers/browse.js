00f7ab04baa7800f72e65a089d200fd28bc3213a
Sfailla <Sfailla1983@gmail.com>
2020-10-28 09:42:04 -0400

    created browse and selectProfile containers

---
+++ src/containers/browse.js
@@ -0,0 +1,5 @@
+import React from 'react';
+
+export default function BrowseContainer({ contentList }) {
+	return <p>Browse Selection...</p>;
+}
---
+++ src/containers/selectProfile.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import { Header } from '../containers';
+
+export default function SelectProfileContainer({ user, setProfile }) {
+	return (
+		<React.Fragment>
+			<Header nav={true} bg={false} />
+		</React.Fragment>
+	);
+}
