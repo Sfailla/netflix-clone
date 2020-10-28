04fcf80527209d44e390821c20a341424cd2b5a8
Sfailla <Sfailla1983@gmail.com>
2020-10-27 21:45:14 -0400

    created custom hook to get data from firestore

---
+++ src/hooks/useCollection.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import { FirebaseContext } from '../firebase';
+
+export default function useCollection(collection) {
+	const [ content, setContent ] = React.useState([]);
+	const [ isLoading, setIsLoading ] = React.useState(false);
+	const { firebase } = React.useContext(FirebaseContext);
+
+	React.useEffect(
+		() => {
+			setIsLoading(true);
+			const unsubscribe = firebase.db.collection(collection).onSnapshot(snapshot => {
+				const collectionData = snapshot.docs.map(doc => {
+					return {
+						id: doc.id,
+						...doc.data()
+					};
+				});
+				setContent(collectionData);
+			});
+			setIsLoading(false);
+			return () => unsubscribe;
+		},
+		[ collection, firebase.db ]
+	);
+
+	return { [collection]: content, isLoading };
+}
