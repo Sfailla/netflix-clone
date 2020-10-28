7e787c6b1a706146ce14ab06f351e715ed2a42f9
Sfailla <Sfailla1983@gmail.com>
2020-10-27 21:24:11 -0400

    refactored homepage route render method to redirect to /browse if user is signed in

--- src/App.js
+++ src/App.js
@@ -1,24 +1,39 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import { BrowserRouter as Router, Redirect, Route, Switch } from 'react-router-dom';
 import * as ROUTES from './constants/routes';
-
 import firebase, { FirebaseContext } from './firebase';
 
 import Home from './pages/Home';
 import SignIn from './pages/Sign_In';
+import Browse from './pages/Browse';
+
+import PrivateRoute from './utils/routes/privateRoute';
+import useAuth from './hooks/useAuth';
 
 function App() {
+	const { isLoading, authUser } = useAuth();
 	const [ register, setRegister ] = React.useState(null);
+
 	return (
 		<Router>
-			<FirebaseContext.Provider value={{ register, setRegister, firebase }}>
+			<FirebaseContext.Provider
+				value={{ register, setRegister, authUser, firebase }}
+			>
 				<Switch>
-					<Route exact path={ROUTES.HOME}>
-						<Home />
-					</Route>
-					<Route exact path={ROUTES.SIGN_IN}>
-						<SignIn />
-					</Route>
+					<Route
+						exact
+						path={ROUTES.HOME}
+						render={() =>
+							isLoading || authUser ? <Redirect to={ROUTES.BROWSE} /> : <Home />}
+					/>
+					<Route exact path={ROUTES.SIGN_IN} component={SignIn} />
+					<PrivateRoute
+						exact
+						isLoading={isLoading}
+						user={authUser}
+						path={ROUTES.BROWSE}
+						component={Browse}
+					/>
 				</Switch>
 			</FirebaseContext.Provider>
 		</Router>
