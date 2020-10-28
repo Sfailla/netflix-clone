219d1e392a93b82a48bb0871db264711883af21e
Sfailla <Sfailla1983@gmail.com>
2020-10-28 09:41:41 -0400

    added hooks to browse page to start pulling in content

--- src/pages/Browse.js
+++ src/pages/Browse.js
@@ -1,9 +1,11 @@
 import React from 'react';
+import BrowseContainer from '../containers/browse';
 import useContent from '../hooks/useContent';
 import selectionFilter from '../utils/filters/selectionFilter';
 
 export default function BrowsePage() {
 	const { movies, series } = useContent();
+	const { contentList } = selectionFilter({ series, movies });
 
-	return <div>this is the browse netflix page</div>;
+	return <BrowseContainer contentList={contentList} />;
 }
