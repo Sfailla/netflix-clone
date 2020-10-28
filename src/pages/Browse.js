e579514ab2605ab60a6c85284e964b705fe3804b
Sfailla <Sfailla1983@gmail.com>
2020-10-27 22:00:24 -0400

    working on browse page

--- src/pages/Browse.js
+++ src/pages/Browse.js
@@ -1,5 +1,9 @@
 import React from 'react';
+import useContent from '../hooks/useContent';
+import selectionFilter from '../utils/filters/selectionFilter';
 
 export default function BrowsePage() {
+	const { movies, series } = useContent();
+
 	return <div>this is the browse netflix page</div>;
 }
