1869484983b20aed9afc95de4b231970530d88ee
Sfailla <Sfailla1983@gmail.com>
2020-10-29 12:42:23 -0400

    removed console.log.  minor changes to routes. added SelectProfile Route component

--- src/App.js
+++ src/App.js
@@ -2,40 +2,51 @@ import React from 'react';
 import { BrowserRouter as Router, Redirect, Route, Switch } from 'react-router-dom';
 import * as ROUTES from './constants/routes';
 import firebase, { FirebaseContext } from './firebase';
+import { Loader } from './components';
 
 import Home from './pages/Home';
 import SignIn from './pages/Sign_In';
 import Browse from './pages/Browse';
+import SelectProfile from './pages/SelectProfile';
 
-import PrivateRoute from './utils/routes/privateRoute';
+import { PrivateRoute } from './utils/routes';
 import useAuth from './hooks/useAuth';
 
 function App() {
-	const { isLoading, authUser } = useAuth();
+	const { isAuthPending, authUser } = useAuth();
 	const [ register, setRegister ] = React.useState(null);
 
 	return (
 		<Router>
-			<FirebaseContext.Provider
-				value={{ register, setRegister, authUser, firebase }}
-			>
-				<Switch>
-					<Route
-						exact
-						path={ROUTES.HOME}
-						render={() =>
-							isLoading || authUser ? <Redirect to={ROUTES.BROWSE} /> : <Home />}
-					/>
-					<Route exact path={ROUTES.SIGN_IN} component={SignIn} />
-					<PrivateRoute
-						exact
-						isLoading={isLoading}
-						user={authUser}
-						path={ROUTES.BROWSE}
-						component={Browse}
-					/>
-				</Switch>
-			</FirebaseContext.Provider>
+			{isAuthPending ? (
+				<Loader />
+			) : (
+				<FirebaseContext.Provider
+					value={{ register, setRegister, authUser, isAuthPending, firebase }}
+				>
+					<Switch>
+						<Route
+							exact
+							path={ROUTES.HOME}
+							render={() =>
+								authUser ? <Redirect to={ROUTES.PROFILE_SELECTION} /> : <Home />}
+						/>
+						<Route path={ROUTES.SIGN_IN} component={SignIn} />
+						<PrivateRoute
+							isLoading={isAuthPending}
+							user={authUser}
+							path={ROUTES.PROFILE_SELECTION}
+							component={SelectProfile}
+						/>
+						<PrivateRoute
+							isLoading={isAuthPending}
+							user={authUser}
+							path={ROUTES.BROWSE}
+							component={Browse}
+						/>
+					</Switch>
+				</FirebaseContext.Provider>
+			)}
 		</Router>
 	);
 }
