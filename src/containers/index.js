cb1823e500340e675e9f15c3f89b0ea302927459
Sfailla <Sfailla1983@gmail.com>
2020-10-29 13:51:10 -0400

    removed SelectUser component from container

--- src/containers/index.js
+++ src/containers/index.js
@@ -4,5 +4,4 @@ export { default as Accordion } from './accordion';
 export { default as Footer } from './footer';
 export { default as SignIn } from './signin';
 export { default as SignUp } from './signup';
-export { default as SelectUser } from './selectUser';
 export { default as Browse } from './browse';
