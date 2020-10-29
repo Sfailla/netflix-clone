4b029fe212e17c50ec88b6b4102152ba7d477c6a
Sfailla <Sfailla1983@gmail.com>
2020-10-29 13:51:55 -0400

    added profile state to firebase context

--- src/pages/SelectProfile.js
+++ src/pages/SelectProfile.js
@@ -1,11 +1,34 @@
 import React from 'react';
-import { Header, SelectUser } from '../containers';
+import { useHistory } from 'react-router-dom';
+import { SelectUser } from '../components';
+import { FirebaseContext } from '../firebase';
+import * as ROUTES from '../constants/routes';
 
-export default function SelectProfile({ setProfile }) {
+export default function SelectProfile() {
+	const history = useHistory();
+	const { authUser, setProfile } = React.useContext(FirebaseContext);
+
 	return (
 		<React.Fragment>
-			<Header />
-			<SelectUser />
+			<SelectUser>
+				<SelectUser.Wrapper>
+					<SelectUser.Title>Whose watching?</SelectUser.Title>
+					<SelectUser.List>
+						<SelectUser.ListItem
+							onClick={() => {
+								setProfile({
+									displayName: authUser.displayName,
+									avatar: authUser.photoURL
+								});
+								history.push(ROUTES.BROWSE);
+							}}
+						>
+							<SelectUser.Avatar src={`/images/user/${authUser.photoURL}`} />
+							<SelectUser.Username>{authUser.displayName}</SelectUser.Username>
+						</SelectUser.ListItem>
+					</SelectUser.List>
+				</SelectUser.Wrapper>
+			</SelectUser>
 		</React.Fragment>
 	);
 }
