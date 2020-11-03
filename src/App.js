d8228df13193a0fbb34775aa3ed282fd4d121a4c
Sfailla <Sfailla1983@gmail.com>
2020-11-02 20:29:28 -0500

    removed console.log statement

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router, Redirect, Route, Switch } from 'react-router-dom';
+import { BrowserRouter as Router, Switch } from 'react-router-dom';
 import * as ROUTES from './constants/routes';
 import firebase, { FirebaseContext } from './firebase';
 import { Loader } from './components';
