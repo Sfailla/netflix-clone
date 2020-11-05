4199e681d470811e4ba09fda9d448330201f2207
Sfailla <Sfailla1983@gmail.com>
2020-11-05 10:10:36 -0500

    added input component and styles

---
+++ src/components/input/index.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import { Container, Form, FormGroup, Input, Label } from './styles/inputStyles';
+
+export default function CustomInput({ children, ...restProps }) {
+	return <Container {...restProps}>{children}</Container>;
+}
+
+CustomInput.Form = function CustomInputForm({ children, ...restProps }) {
+	return <Form {...restProps}>{children}</Form>;
+};
+
+CustomInput.FormGroup = function CustomInputFormGroup({ children, ...restProps }) {
+	return <FormGroup {...restProps}>{children}</FormGroup>;
+};
+
+CustomInput.Input = function MyInput({ children, ...restProps }) {
+	return <Input {...restProps}>{children}</Input>;
+};
+
+CustomInput.Label = function CustomInputLabel({ children, ...restProps }) {
+	return <Label {...restProps}>{children}</Label>;
+};
