62382d81d76e46845a459f85028b46065db89d86
Sfailla <Sfailla1983@gmail.com>
2020-11-07 15:04:31 -0500

    added index file for hooks folder

---
+++ src/hooks/index.js
@@ -0,0 +1,15 @@
+import useAuth from './useAuth';
+import useClickOutsideRef from './useClickOutsideRef';
+import useCollection from './useCollection';
+import useContent from './useContent';
+import useForm from './useForm';
+import useWindowSize from './useWindowSize';
+
+export {
+	useAuth,
+	useClickOutsideRef,
+	useCollection,
+	useContent,
+	useForm,
+	useWindowSize
+};
