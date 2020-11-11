29e868babd3f3ddba8f68ffd34090ab483ad1068
Sfailla <Sfailla1983@gmail.com>
2020-11-11 09:40:37 -0500

    added helpers folder to utils. added maturityRating helper to transform rating numbers to standard movie ratings

---
+++ src/utils/helpers/maturityRating.js
@@ -0,0 +1,11 @@
+export const maturityRating = rating => {
+	let contentRating;
+	if (rating <= 12) {
+		contentRating = 'PG';
+	} else if (rating == 15) {
+		contentRating = 'PG-13';
+	} else if (rating >= 18) {
+		contentRating = 'TV-MA';
+	}
+	return contentRating;
+};
