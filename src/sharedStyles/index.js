cddc0befb93341f0fc1072b387302ee065d836db
Sfailla <Sfailla1983@gmail.com>
2020-11-12 14:58:37 -0500

    minor changes

--- src/sharedStyles/index.js
+++ src/sharedStyles/index.js
@@ -3,8 +3,8 @@ import styled from 'styled-components/macro';
 export const Container = styled.div`
 	width: 100%;
 	height: 100%;
-	overflow-x: hidden;
 	padding: 0 56px;
+	overflow-x: hidden;
 
 	@media (max-width: 700px) {
 		padding: 0 10px;
