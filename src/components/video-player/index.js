b4704c13125e4fb38ff9b6e7f7ab4575a103a47f
Sfailla <Sfailla1983@gmail.com>
2020-11-19 21:37:43 -0500

    created video player and styles for trailers

---
+++ src/components/video-player/index.js
@@ -0,0 +1,71 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+import {
+	Container,
+	Button,
+	Overlay,
+	Inner,
+	Close,
+	Iframe
+} from './styles/videoPlayerStyles';
+
+const defaultVideoPlayerState = {
+	showplayer: null,
+	setShowPlayer: () => {}
+};
+
+const VideoPlayerContext = React.createContext(defaultVideoPlayerState);
+
+export default function VideoPlayer({ children, ...restProps }) {
+	const [ showPlayer, setShowPlayer ] = React.useState(false);
+
+	return (
+		<VideoPlayerContext.Provider value={{ showPlayer, setShowPlayer }}>
+			<Container {...restProps}>{children}</Container>
+		</VideoPlayerContext.Provider>
+	);
+}
+
+VideoPlayer.Iframe = function VideoPlayerIframe({ ...restProps }) {
+	return <Iframe {...restProps} />;
+};
+
+const YouTube = ({ video, autoplay, rel, modest }) => {
+	const videoSrc = `https://www.youtube.com/embed/${video}?autoplay=${autoplay}&rel=${rel}&modestbranding=${modest}&showinfo=0`;
+	return (
+		<Iframe
+			title="YTplayer"
+			type="text/html"
+			width="100%"
+			height="100%"
+			src={videoSrc}
+			frameborder="0"
+		/>
+	);
+};
+
+VideoPlayer.Video = function VideoPlayerVideo({ trailer }) {
+	const { showPlayer, setShowPlayer } = React.useContext(VideoPlayerContext);
+
+	return showPlayer
+		? ReactDOM.createPortal(
+				<Overlay onClick={() => setShowPlayer(false)}>
+					<Inner>
+						<YouTube video={trailer} autoplay="1" rel="0" showInfo="0" modest="1" />
+					</Inner>
+				</Overlay>,
+				document.body
+			)
+		: null;
+};
+
+VideoPlayer.Button = function VideoPlayerButton({ ...restProps }) {
+	const { setShowPlayer } = React.useContext(VideoPlayerContext);
+
+	return (
+		<Button onClick={() => setShowPlayer(prevState => !prevState)} {...restProps}>
+			Play now
+		</Button>
+	);
+};
---
+++ src/components/video-player/styles/videoPlayerStyles.js
@@ -0,0 +1,76 @@
+import styled from 'styled-components/macro';
+
+export const Container = styled.div``;
+
+export const Button = styled.button`
+	background-color: transparent;
+	color: #fff;
+	border: 2px solid;
+	border-color: #e6e6e6;
+	outline: 0;
+	padding: 10px 20px;
+	border-radius: 5px;
+	max-width: 130px;
+	font-size: 20px;
+	margin-top: 10px;
+	display: flex;
+	justify-content: center;
+	align-items: center;
+	cursor: pointer;
+	transition: border-color 0.3s ease, color 0.3s ease;
+
+	&:hover {
+		border-color: #ff1e1e;
+		color: #ff1e1e;
+	}
+
+	@media (max-width: 850px) {
+		width: 100px;
+		height: 30px;
+		font-size: 12px;
+	}
+`;
+
+export const Overlay = styled.div`
+	width: 100%;
+	height: 100%;
+	display: flex;
+	justify-content: center;
+	position: fixed;
+	top: 0;
+	left: 0;
+	right: 0;
+	bottom: 0;
+	background: rgba(0, 0, 0, 0.9);
+	margin: 0 20px;
+	z-index: 50;
+`;
+
+export const Inner = styled.div`
+	width: 100%;
+	max-width: 900px;
+	position: relative;
+	margin: 0 auto;
+
+	& iframe {
+		width: 100%;
+		height: 50%;
+		position: absolute;
+		top: 50%;
+		left: 50%;
+		transform: translate(-50%, -50%);
+	}
+`;
+
+export const Close = styled.div`
+	width: 25px;
+	height: 25px;
+	positon: absolute;
+	right: 15px;
+	top: 15px;
+	cursor: pointer;
+
+	background-color: lightpink;
+`;
+
+export const Iframe = styled.iframe``;
