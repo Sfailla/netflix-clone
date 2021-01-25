1e3096e3c9f07cda3df657b5294c36c553067dfc
Sfailla <Sfailla1983@gmail.com>
2021-01-25 16:26:04 -0500

    fixind deployment issue

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -1,39 +1,37 @@
+import firebaseConfig from './config';
 import app from 'firebase/app';
 import 'firebase/auth';
 import 'firebase/firestore';
-import firebaseConfig from './config';
 import randomAvatar from '../utils/helpers/randomAvatar';
-
-randomAvatar();
 
 // Initialize Firebase
 class Firebase {
-	constructor() {
-		app.initializeApp(firebaseConfig);
-		this.auth = app.auth();
-		this.db = app.firestore();
-	}
+  constructor() {
+    app.initializeApp(firebaseConfig);
+    this.auth = app.auth();
+    this.db = app.firestore();
+  }
 
-	async register(email, name, password) {
-		const newUser = await this.auth.createUserWithEmailAndPassword(email, password);
+  async register(email, name, password) {
+    const newUser = await this.auth.createUserWithEmailAndPassword(email, password);
 
-		return await newUser.user.updateProfile({
-			displayName: name.toLowerCase(),
-			photoURL: randomAvatar()
-		});
-	}
+    return await newUser.user.updateProfile({
+      displayName: name.toLowerCase(),
+      photoURL: randomAvatar(),
+    });
+  }
 
-	async login(email, password) {
-		return await this.auth.signInWithEmailAndPassword(email, password);
-	}
+  async login(email, password) {
+    return await this.auth.signInWithEmailAndPassword(email, password);
+  }
 
-	async logout() {
-		await this.auth.signOut();
-	}
+  async logout() {
+    await this.auth.signOut();
+  }
 
-	async resetPassword(email) {
-		await this.auth.sendPasswordResetEmail(email);
-	}
+  async resetPassword(email) {
+    await this.auth.sendPasswordResetEmail(email);
+  }
 }
 
 const firebase = new Firebase();
